70b9966f6519ace1478fd3c7efdf1489b0783052
João Costa <joaoabelcosta3@gmail.com>
2024-05-26 23:42:06 +0100

    Setup of Navigation

---
+++ src/screens/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from './styles';
+
+export const Landing: React.FC = () => {
+  return (
+    <Container>
+      <Text>Hello World</Text>
+    </Container>
+  );
+};
